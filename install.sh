d2f2c74dddccc45043747cc442b15180c0b29520
maaru <maratarzymatov288@gmail.com>
2025-01-02 08:55:41 +0600

    update inst

--- install.sh
+++ install.sh
@@ -1,69 +1,35 @@
-#!/bin/zsh
+#!/bin/bash
 
-# Определение переменных
-HOME_DIR="$HOME"
-BACKUP_DIR="$HOME_DIR/zsh.bak"
-REPO_DIR="$HOME_DIR/zinit.zsh-maaru"
-REPO_URL="https://github.com/maarutan/zinit.zsh-maaru"
-
-# Создание каталога для бэкапов, если его нет
-if [ ! -d "$BACKUP_DIR" ]; then
-    mkdir -p "$BACKUP_DIR"
-    echo "Создан каталог для бэкапов: $BACKUP_DIR"
-fi
-
-# Клонирование репозитория
-if [ -d "$REPO_DIR" ]; then
-    echo "Репозиторий уже клонирован: $REPO_DIR"
-else
-    git clone "$REPO_URL" "$REPO_DIR"
-    echo "Клонирован репозиторий: $REPO_DIR"
-fi
-
-# Перемещение файлов из репозитория в домашний каталог
-for FILE in "$REPO_DIR/.zshrc" "$REPO_DIR/.p10k.zsh" "$REPO_DIR/.zsh"; do
-    BASENAME=$(basename "$FILE")
-    TARGET_FILE="$HOME_DIR/$BASENAME"
-    if [ -e "$TARGET_FILE" ]; then
-        # Если файл уже существует, переименовываем в .bak и переносим
-        mv "$TARGET_FILE" "$BACKUP_DIR/${BASENAME}.bak"
-        echo "Перемещен в бэкап: $TARGET_FILE -> $BACKUP_DIR/${BASENAME}.bak"
+# Backup existing files if they exist
+for file in "$HOME/.zshrc" "$HOME/.p10k.zsh" "$HOME/.zsh"; do
+    if [ -e "$file" ]; then
+        backup="$file.bak"
+        echo "Backing up existing file $file to $backup"
+        mv "$file" "$backup"
     fi
-    # Копируем файл из репозитория в домашний каталог
-    if [ -e "$FILE" ]; then
-        cp "$FILE" "$HOME_DIR"
-        echo "Файл скопирован: $FILE -> $HOME_DIR"
-    fi
-
 done
 
-# Установка Zinit
-if command -v zinit > /dev/null 2>&1; then
-    echo "Zinit уже установлен. Обновляем .zshrc..."
-    source "$HOME_DIR/.zshrc"
+# Move new configuration files
+mv $HOME/.zinit.zsh-maaru/.zshrc  $HOME/.zshrc
+mv $HOME/.zinit.zsh-maaru/.p10k.zsh  $HOME/.p10k.zsh
+mv $HOME/.zinit.zsh-maaru/.zsh/ $HOME/.zsh
+
+sleep  2
+
+# Check and install Zinit
+if [ -d "$HOME/.zinit/bin" ]; then
+    echo "Zinit is already installed on your system."
 else
-    echo "Zinit не найден. Устанавливаем..."
-    cd "$REPO_DIR" || exit
-    ./install.sh
-    cd "$HOME_DIR" || exit
-    echo "Zinit установлен. Обновляем .zshrc..."
-    source "$HOME_DIR/.zshrc"
+    echo "Zinit is not installed. Installing Zinit..."
+
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma-continuum/zinit/main/doc/install.sh)"
+
+    if [ $? -eq 0 ]; then
+        echo "Zinit has been successfully installed."
+    else
+        echo "Failed to install Zinit. Please check your internet connection or permissions."
+        exit 1
+    fi
 fi
 
-# Проверка и обновление Zsh
-if command -v zsh > /dev/null 2>&1; then
-    echo "Zsh уже установлен. Обновляем..."
-    source "$HOME_DIR/.zshrc"
-else
-    echo "Zsh не найден. Устанавливаем..."
-    if [ -x "$(command -v apt-get)" ]; then
-        sudo apt-get update && sudo apt-get install zsh -y
-    Элиф  [  -х   " $(command -v brew) "  ]; then
-        brew install zsh 
-     еще 
-        эхо  "Пакетный менеджер не найден. Установите Zsh вручную."
-        выход   1 
-    фи 
-    эхо  "Zsh установлен. Обновляем..."
-    источник   " $HOME_DIR/.zshrc"
-fi
+source $HOME/.zshrc
